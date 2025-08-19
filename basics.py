afa4f99ecf1212522072eb0151de3a73ff52e18c
PresidentOgbu <boluogbu@gmail.com>
2025-08-19 18:36:58 +0100

    lemaa lecture update

---
+++ basics.py
@@ -0,0 +1,15 @@
+# print (Hello, world)
+
+
+# name = "uchenna"
+# print (name) 
+
+print ("The best school in Nigeria is Havard University")
+
+
+school = "UNILAG is the best institute of learning in nigeria"
+print (school)
+
+
+
+
