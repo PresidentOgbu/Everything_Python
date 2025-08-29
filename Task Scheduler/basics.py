b8403caf375ead737505e04fdb6e52d5c8a7b352
PresidentOgbu <boluogbu@gmail.com>
2025-08-29 23:37:20 +0100

    updated script

--- Task Scheduler/basics.py
+++ Task Scheduler/basics.py
@@ -181,7 +181,7 @@
 
 
 
-# for letter in "boy":
+# for letter in "python":
 #     print(letter)   
 
 
@@ -191,6 +191,28 @@ index = 0
 while index < len(word):
     print(word[index])
 index = index + 1
+
+
+
+# NESTED LOOPS
+for n in range(1, 4):
+for j in range(4, 7):
+print(f"n = {n} and j = {j}")
+
+
+
+
+word = "Python"
+index = 0
+while index < len(word):
+print(word[index])
+index = index + 1
+
+
+
+
+
+
 
 
 
@@ -209,27 +231,42 @@ a separate line. Here is some sample output:
 
 
 
+1. for n in range(2, 11):
+   print(n)
 
-In this challenge, you will write a program called invest.py that tracks
-the growing amount of an investment over time.
-An initial deposit, called the principal amount, is made. Each year,
-the amount increases by a fixed percentage, called the annual rate of
-return.
-For example, a principal amount of $100 with an annual rate of return
-of 5% increases the first year by $5. The second year, the increase is
-5% of the new amount $105, which is $5.25.
-Write a function called invest with three parameters: the principal
-amount, the annual rate of return, and the number of years to calculate.
-The function signature might look something like this:
-def invest(amount, rate, years):
-The function then prints out the amount of the investment, rounded
-to 2 decimal places, at the end of each year for the specified number
-of years.
-For example, calling invest(100, .05, 4) should print the following:
-year 1: $105.00
-year 2: $110.25
-year 3: $115.76
-year 4: $121.55
-To finish the program, prompt the user to enter an initial amount, an
-annual percentage rate, and a number of years. Then call invest() to
-display the calculations for the values entered by the user.
+2. i = 2
+   while i <= 10:
+       print(i)
+       i += 1
+
+3. def doubles(n):
+       return n * 2
+
+   result = 2
+   for _ in range(3):
+       result = doubles(result)
+       print(result)
+
+# In this challenge, you will write a program called invest.py that tracks
+# the growing amount of an investment over time.
+# An initial deposit, called the principal amount, is made. Each year,
+# the amount increases by a fixed percentage, called the annual rate of
+# return.
+# For example, a principal amount of $100 with an annual rate of return
+# of 5% increases the first year by $5. The second year, the increase is
+# 5% of the new amount $105, which is $5.25.
+# Write a function called invest with three parameters: the principal
+# amount, the annual rate of return, and the number of years to calculate.
+# The function signature might look something like this:
+# def invest(amount, rate, years):
+# The function then prints out the amount of the investment, rounded
+# to 2 decimal places, at the end of each year for the specified number
+# of years.
+# For example, calling invest(100, .05, 4) should print the following:
+# year 1: $105.00
+# year 2: $110.25
+# year 3: $115.76
+# year 4: $121.55
+# To finish the program, prompt the user to enter an initial amount, an
+# annual percentage rate, and a number of years. Then call invest() to
+# display the calculations for the values entered by the user.
